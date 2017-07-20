60398a29c7a0484fdd5e234a0519cb47f8707fa1
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2017-07-21 02:46:32 +0300

    good test wrote

--- src/test/java/log/example/CarTest.java
+++ src/test/java/log/example/CarTest.java
@@ -3,6 +3,10 @@ package log.example;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static log.example.TestUtil.fromSystemOut;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 /**
  * Created by Roman_Iovlev on 7/19/2017.
  */
@@ -17,7 +21,11 @@ public class CarTest {
 
     @Test
     void driveTest() {
-        car.drive();
-        car.stop();
+        assertThat(fromSystemOut(car::drive), is("drive method executed\n"));
+    }
+
+    @Test
+    void stopTest() {
+        assertThat(fromSystemOut(car::stop), is("stop method executed\n"));
     }
 }
